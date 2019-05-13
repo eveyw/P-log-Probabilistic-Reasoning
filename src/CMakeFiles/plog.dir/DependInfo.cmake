5c282cd246973789747c8bf676bd8654bfa7b7a7
Wen Yang <wyang41@binghamton.edu>
2019-05-13 03:30:45 -0400

    code

---
+++ src/CMakeFiles/plog.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/eveyw/Desktop/plog/src/AnswerSet.cpp" "/Users/eveyw/Desktop/plog/src/CMakeFiles/plog.dir/AnswerSet.cpp.o"
+  "/Users/eveyw/Desktop/plog/src/Attribute.cpp" "/Users/eveyw/Desktop/plog/src/CMakeFiles/plog.dir/Attribute.cpp.o"
+  "/Users/eveyw/Desktop/plog/src/Formula.cpp" "/Users/eveyw/Desktop/plog/src/CMakeFiles/plog.dir/Formula.cpp.o"
+  "/Users/eveyw/Desktop/plog/src/GroundLiteral.cpp" "/Users/eveyw/Desktop/plog/src/CMakeFiles/plog.dir/GroundLiteral.cpp.o"
+  "/Users/eveyw/Desktop/plog/src/GroundProgram.cpp" "/Users/eveyw/Desktop/plog/src/CMakeFiles/plog.dir/GroundProgram.cpp.o"
+  "/Users/eveyw/Desktop/plog/src/GroundRule.cpp" "/Users/eveyw/Desktop/plog/src/CMakeFiles/plog.dir/GroundRule.cpp.o"
+  "/Users/eveyw/Desktop/plog/src/GroundTerm.cpp" "/Users/eveyw/Desktop/plog/src/CMakeFiles/plog.dir/GroundTerm.cpp.o"
+  "/Users/eveyw/Desktop/plog/src/Grounder.cpp" "/Users/eveyw/Desktop/plog/src/CMakeFiles/plog.dir/Grounder.cpp.o"
+  "/Users/eveyw/Desktop/plog/src/Literal.cpp" "/Users/eveyw/Desktop/plog/src/CMakeFiles/plog.dir/Literal.cpp.o"
+  "/Users/eveyw/Desktop/plog/src/Parse.cpp" "/Users/eveyw/Desktop/plog/src/CMakeFiles/plog.dir/Parse.cpp.o"
+  "/Users/eveyw/Desktop/plog/src/Plog.cpp" "/Users/eveyw/Desktop/plog/src/CMakeFiles/plog.dir/Plog.cpp.o"
+  "/Users/eveyw/Desktop/plog/src/Program.cpp" "/Users/eveyw/Desktop/plog/src/CMakeFiles/plog.dir/Program.cpp.o"
+  "/Users/eveyw/Desktop/plog/src/Query.cpp" "/Users/eveyw/Desktop/plog/src/CMakeFiles/plog.dir/Query.cpp.o"
+  "/Users/eveyw/Desktop/plog/src/RandomRule.cpp" "/Users/eveyw/Desktop/plog/src/CMakeFiles/plog.dir/RandomRule.cpp.o"
+  "/Users/eveyw/Desktop/plog/src/Rule.cpp" "/Users/eveyw/Desktop/plog/src/CMakeFiles/plog.dir/Rule.cpp.o"
+  "/Users/eveyw/Desktop/plog/src/Sort.cpp" "/Users/eveyw/Desktop/plog/src/CMakeFiles/plog.dir/Sort.cpp.o"
+  "/Users/eveyw/Desktop/plog/src/State.cpp" "/Users/eveyw/Desktop/plog/src/CMakeFiles/plog.dir/State.cpp.o"
+  "/Users/eveyw/Desktop/plog/src/TermTree.cpp" "/Users/eveyw/Desktop/plog/src/CMakeFiles/plog.dir/TermTree.cpp.o"
+  "/Users/eveyw/Desktop/plog/src/TrEngine.cpp" "/Users/eveyw/Desktop/plog/src/CMakeFiles/plog.dir/TrEngine.cpp.o"
+  "/Users/eveyw/Desktop/plog/src/Variable.cpp" "/Users/eveyw/Desktop/plog/src/CMakeFiles/plog.dir/Variable.cpp.o"
+  "/Users/eveyw/Desktop/plog/src/global.cpp" "/Users/eveyw/Desktop/plog/src/CMakeFiles/plog.dir/global.cpp.o"
+  "/Users/eveyw/Desktop/plog/src/pgEngine.cpp" "/Users/eveyw/Desktop/plog/src/CMakeFiles/plog.dir/pgEngine.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/CMakeFiles/plog.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/plog.dir/Plog.cpp.o"
+  "CMakeFiles/plog.dir/AnswerSet.cpp.o"
+  "CMakeFiles/plog.dir/Attribute.cpp.o"
+  "CMakeFiles/plog.dir/Formula.cpp.o"
+  "CMakeFiles/plog.dir/GroundLiteral.cpp.o"
+  "CMakeFiles/plog.dir/GroundProgram.cpp.o"
+  "CMakeFiles/plog.dir/GroundRule.cpp.o"
+  "CMakeFiles/plog.dir/GroundTerm.cpp.o"
+  "CMakeFiles/plog.dir/Grounder.cpp.o"
+  "CMakeFiles/plog.dir/Literal.cpp.o"
+  "CMakeFiles/plog.dir/Parse.cpp.o"
+  "CMakeFiles/plog.dir/Program.cpp.o"
+  "CMakeFiles/plog.dir/Query.cpp.o"
+  "CMakeFiles/plog.dir/RandomRule.cpp.o"
+  "CMakeFiles/plog.dir/Rule.cpp.o"
+  "CMakeFiles/plog.dir/Sort.cpp.o"
+  "CMakeFiles/plog.dir/State.cpp.o"
+  "CMakeFiles/plog.dir/TermTree.cpp.o"
+  "CMakeFiles/plog.dir/TrEngine.cpp.o"
+  "CMakeFiles/plog.dir/Variable.cpp.o"
+  "CMakeFiles/plog.dir/global.cpp.o"
+  "CMakeFiles/plog.dir/pgEngine.cpp.o"
+  "plog.pdb"
+  "plog"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/plog.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
